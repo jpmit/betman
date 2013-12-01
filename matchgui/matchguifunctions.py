2ed2f5de306ad21838c3d4e6cea62afb8dded6fb
James Mithen <jamesmithen@gmail.com>
2013-12-01 19:26:53 +0000

    fixed layout of price panel on GUI

--- matchgui/matchguifunctions.py
+++ matchgui/matchguifunctions.py
@@ -6,8 +6,16 @@ import betman.matchmarkets.marketmatcher as marketmatcher
 import betman.matchmarkets.matchconst as matchconst
 from betman import database, const
 
+class GuiError(Exception):
+    pass
+
 # cache a list of matching markets for each event
 MATCH_CACHE = {}
+
+# matching selections keys are bdaq mid, values are [(bdaq_sid1,
+# bf_sid1), ...] where bdaq_sid1 and bf_sid1 are matching selection
+# ids.  Note the order is the same as displayed on the BDAQ website.
+MATCHING_SELECTIONS_CACHE = {}
 
 # cache the event list
 BDAQ_EVENTS = []
@@ -15,6 +23,7 @@ BF_EVENTS = []
 
 def _sort_match(item):
     """Sort matching markets by BDAQ start time"""
+    
     return item[0].starttime
 
 def market_name(bdaqename, index):
@@ -23,21 +32,64 @@ def market_name(bdaqename, index):
     global MATCH_CACHE
     return MATCH_CACHE[bdaqename][index][0].name
 
+def display_order(bdaqmid):
+    minfo = bdaqapi.GetMarketInformation([bdaqmid])
+    order = [None]*len(minfo.Markets.Selections)
+    for m in minfo.Markets.Selections:
+        order[m._DisplayOrder - 1] = m._Id
+    return order
+
 def market_prices(bdaqename, index):
-    global MATCH_CACHE
+    """
+    Return bdaqsels, bfsels, lists of selection objects for BDAQ and
+    BF respectively.
+    """
+    
+    global MATCH_CACHE, MATCHING_SELECTIONS_CACHE
 
     # get bdaq and bf market
     bdaqmark, bfmark = MATCH_CACHE[bdaqename][index]
 
-    # get prices from api
-    bdaqsels = bdaqapi.GetPrices_nApi([bdaqmark.id])
-    bfsels = bfapi.GetPrices_nApi([bfmark.id])
-
-    # should return something here if we didn't get any market
-    # information; in this case bdaqsels[1] and bfsels[1] will be
+    # get prices from api: NB should check here that we actually got
+    # selections.  If we didn't, bdaqsels[1] and bfsels[1] will be
     # non-empty (they will contain the single market id only).
-    # Returned below is a list of selections
-    return bdaqsels[0].values()[0].values(), bfsels[0].values()[0].values()
+    # Returned below is a dictionary of selections with keys that are
+    # the sids.
+    bdaqsels = bdaqapi.GetPrices_nApi([bdaqmark.id])[0].values()[0]
+    bfsels = bfapi.GetPrices_nApi([bfmark.id])[0].values()[0]
+    
+    if bdaqmark.id not in MATCHING_SELECTIONS_CACHE:
+
+        # get lists of selections
+        bdaqsellist = bdaqsels.values()
+        bfsellist = bfsels.values()
+            
+        # get matching selections
+        msels =  marketmatcher.get_match_selections([bdaqsellist],
+                                                    [bfsellist])
+
+        # create dict with bdaq sid as key, bf sid as value
+        mseldict = {k.id : v.id for (k, v) in msels}
+
+        # get the BDAQ display order of selections
+        dorder = display_order(bdaqmark.id)
+
+        # add sorting info to the cache
+        mlist = []
+        for d in dorder:
+            # note d will not be in mseldict if we failed to match the
+            # selection!
+            if d in mseldict:
+                mlist.append((d, mseldict[d]))
+        # create cache entry
+        MATCHING_SELECTIONS_CACHE[bdaqmark.id] = mlist
+
+    # get selection ordering from cache
+    osids = MATCHING_SELECTIONS_CACHE[bdaqmark.id]    
+
+    bdaqorder = [bdaqsels[s[0]] for s in osids]
+    bforder = [bfsels[s[1]] for s in osids]
+    return bdaqorder, bforder
 
 def match_markets(bdaqename):
     global BDAQ_EVENTS, BF_EVENTS, MATCH_CACHE
@@ -73,9 +125,11 @@ def match_markets(bdaqename):
     # markets.
     bdaqsels = bdaqapi.GetPrices([m[0].id for m in matchmarks])
     for (i, sels) in enumerate(bdaqsels):
-        matchmarks[i][0].properties['totalmatched'] = sels[0].properties['totalmatched']
+        matchmarks[i][0].properties['totalmatched'] = sels[0].\
+                                                      properties['totalmatched']
 
-    # sort matching markets by starttime
+    # sort matching markets by starttime; NB could sort them by the
+    # BDAQ official display order at some point if necessary.
     matchmarks.sort(key=_sort_match)
 
     MATCH_CACHE[bdaqename] = matchmarks
--- matchgui/pricepanel.py
+++ matchgui/pricepanel.py
@@ -3,13 +3,19 @@ import wx.lib.scrolledpanel as scrolledpanel
 import matchguifunctions
 
 # colours for the buttons
-LBLUE = (14, 218, 249)
-LPINK = (219, 162, 197)
-GREY = (86, 86, 86)
-LGREEN = (151, 226, 176)
+LBLUE   = (14, 218, 249)
+LPINK   = (219, 162, 197)
+GREY    = (86, 86, 86)
+LGREEN  = (151, 226, 176)
 LYELLOW = (235, 234, 194)
 
 class PricePanel(scrolledpanel.ScrolledPanel):
+    # constants for the layout
+    BSIZE = (50, 50) # button size (that displays odds and stakes)
+    VGAP = 0
+    HGAP = 0
+    BPAD = 0 # padding around button
+    BLSPACING = (10, 10) # spacing between back and lay prices
 
     def __init__(self, parent):
 
@@ -33,7 +39,7 @@ class PricePanel(scrolledpanel.ScrolledPanel):
 
         # get market name
         name = matchguifunctions.market_name(self.event, self.index)
-        name_sz.Add(wx.StaticText(self, label=name))
+        name_sz.Add(wx.StaticText(self, label = name))
         main_sizer.Add(name_sz)
         main_sizer.AddSpacer(50)
 
@@ -41,40 +47,98 @@ class PricePanel(scrolledpanel.ScrolledPanel):
         bdaqsels, bfsels = matchguifunctions.market_prices(self.event,
                                                            self.index)
 
-        sz = wx.GridSizer(rows=len(bdaqsels), vgap = 1, hgap = 1)
-        for sel in bdaqsels:
-            sz.Add(wx.StaticText(self, label = sel.name, style = wx.ALIGN_CENTER))
+        # go through each selection in turn and add info to the grid
+        # cells.
+        for bdaqsel, bfsel in zip(bdaqsels, bfsels):
 
-            # list of plate buttons
+            sel_sizer = wx.BoxSizer(wx.HORIZONTAL)
+            sel_gridsz = wx.GridSizer(rows = 2, vgap = PricePanel.VGAP,
+                                      hgap = PricePanel.HGAP)
+            
+            # As the name of the selection we display the BDAQ name NB
+            # struggling to center this text! (it always appears in
+            # the top left).
+            name_sizer = wx.BoxSizer(wx.VERTICAL)
+            selname = wx.StaticText(self, label = bdaqsel.name,
+                                    size = (PricePanel.BSIZE[0]*2,
+                                            PricePanel.BSIZE[1]*2),
+                                    style = wx.ALIGN_CENTER)
+            selname.Wrap(2*PricePanel.BSIZE[0])
+            name_sizer.AddStretchSpacer()
+            name_sizer.Add(selname, 0, wx.CENTER)
+            name_sizer.AddStretchSpacer()
+            sel_sizer.Add(name_sizer, 0, wx.CENTER)
+            
+            # list of buttons
             pbuttons = []
-            bprices = sel.padback[:3]
-            bprices.reverse()
-            lprices = sel.padlay[:3]
-            for odds, stake in bprices:
-                bt = wx.Button(self, size = (50, 50))
-                #bt.SetSize((100, 30))
+            bdaqbprices = bdaqsel.padback[:3]
+            bdaqbprices.reverse()
+            bdaqlprices = bdaqsel.padlay[:3]
+            
+            for odds, stake in bdaqbprices:
+                bt = wx.Button(self, size = PricePanel.BSIZE)
+                if odds is None:
+                    bt.SetBackgroundColour(GREY)
+                else:
+                    bt.SetBackgroundColour(LYELLOW)
+                    bt.SetLabel('{0}\n{1}'.format(odds, stake))
+                pbuttons.append((bt, 0, wx.ALL, PricePanel.BPAD))
+
+            sel_gridsz.AddMany(pbuttons)
+            # add spacing between back and lay prices
+            sel_gridsz.AddSpacer(PricePanel.BLSPACING)
+
+            pbuttons = []
+            for odds, stake in bdaqlprices:
+                bt = wx.Button(self, size = PricePanel.BSIZE)
+                if odds is None:
+                    bt.SetBackgroundColour(GREY)
+                else:
+                    bt.SetBackgroundColour(LGREEN)
+                    bt.SetLabel('{0}\n{1}'.format(odds, stake))
+                pbuttons.append((bt, 0, wx.ALL, PricePanel.BPAD))
+            
+            sel_gridsz.AddMany(pbuttons)
+
+            # list of buttons
+            pbuttons = []
+            bfbprices = bfsel.padback[:3]
+            bfbprices.reverse()
+            bflprices = bfsel.padlay[:3]
+            
+            for odds, stake in bfbprices:
+                bt = wx.Button(self, size = PricePanel.BSIZE)
                 if odds is None:
                     bt.SetBackgroundColour(GREY)
                 else:
                     bt.SetBackgroundColour(LBLUE)
                     bt.SetLabel('{0}\n{1}'.format(odds, stake))
-                pbuttons.append((bt, 0, wx.ALL, 5))
-            
-            for odds, stake in lprices:
-                bt = wx.Button(self, size=(50, 50))
-                           #           style = pbtn.PB_STYLE_SQUARE, size = (100, 30))
-                #bt.SetSize((100, 30))                
+                pbuttons.append((bt, 0, wx.ALL, PricePanel.BPAD))
+
+            sel_gridsz.AddMany(pbuttons)
+            # add spacing between back and lay prices
+            sel_gridsz.AddSpacer((20,20))
+
+            pbuttons = []
+            for odds, stake in bflprices:
+                bt = wx.Button(self, size = PricePanel.BSIZE)
                 if odds is None:
                     bt.SetBackgroundColour(GREY)
                 else:
                     bt.SetBackgroundColour(LPINK)
                     bt.SetLabel('{0}\n{1}'.format(odds, stake))
-                pbuttons.append((bt, 0, wx.ALL, 5))
-            
-            sz.AddMany(pbuttons)
+                pbuttons.append((bt, 0, wx.ALL, PricePanel.BPAD))
 
-        # add the grid to the main sizer
-        main_sizer.Add(sz)
+            sel_gridsz.AddMany(pbuttons)
+
+            # add grid to selection sizer
+            sel_sizer.Add(sel_gridsz)
+
+            # add horiz sizer to the main sizer
+            main_sizer.Add(sel_sizer)
+
+            # space before next selection prices
+            main_sizer.AddSpacer(20)            
                     
         self.SetSizer(main_sizer)
         self.SetupScrolling()
