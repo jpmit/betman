ed5b7fa749e81410d15ac650c98b5a04fc915aa7
James Mithen <jamesmithen@gmail.com>
2014-02-01 10:06:33 +0000

    added automation functionality

---
+++ matchgui/automation.py
@@ -0,0 +1,14 @@
+class Automation(object):
+
+    def update(self, stratgroup):
+        """Add or remove strategies from strat, which is the global strategy
+        group.
+
+        We may want to do quite a bit of work here.  For example, our
+        our automation could be for market making on horse races
+        during the 10 minutes, for the favourite only.  This function
+        is responsible for adding/removing strategies from stratgroup
+        in accordance with this policy.
+        """
+        
+        pass
---
+++ matchgui/automation/hautomation.py
@@ -0,0 +1,4 @@
+from automation import Automation
+
+class MyAutomation(Automation):
+    pass
